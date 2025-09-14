fb37344ddd269bca0eda915f5977c56723f1ddb7
pzr2335104343@qq.com <233510434@qq.com>
2025-09-14 08:51:08 +0800

    对接后端API

---
+++ ai-code-monther-frontend/src/api/appController.ts
@@ -0,0 +1,168 @@
+// @ts-ignore
+/* eslint-disable */
+import request from '@/request'
+
+/** 此处后端没有提供注释 POST /app/add */
+export async function addApp(body: API.AppAddRequest, options?: { [key: string]: any }) {
+  return request<API.BaseResponseLong>('/app/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 此处后端没有提供注释 POST /app/admin/delete */
+export async function deleteAppByAdmin(body: API.DeleteRequest, options?: { [key: string]: any }) {
+  return request<API.BaseResponseBoolean>('/app/admin/delete', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 此处后端没有提供注释 POST /app/admin/list/page/vo */
+export async function listAppVoByPageByAdmin(
+  body: API.AppQueryRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePageAppVO>('/app/admin/list/page/vo', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 此处后端没有提供注释 POST /app/admin/update */
+export async function updateAppByAdmin(
+  body: API.AppAdminUpdateRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseBoolean>('/app/admin/update', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 此处后端没有提供注释 GET /app/chat/gen/code */
+export async function chatGenCode(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.chatGenCodeParams,
+  options?: { [key: string]: any }
+) {
+  return request<API.ServerSentEventString[]>('/app/chat/gen/code', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  })
+}
+
+/** 此处后端没有提供注释 POST /app/delete/my */
+export async function deleteMyApp(body: API.DeleteRequest, options?: { [key: string]: any }) {
+  return request<API.BaseResponseBoolean>('/app/delete/my', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 此处后端没有提供注释 POST /app/deploy */
+export async function deployApp(body: API.AppDeployRequest, options?: { [key: string]: any }) {
+  return request<API.BaseResponseString>('/app/deploy', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 此处后端没有提供注释 GET /app/get */
+export async function getAppVoByIdByAdmin(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getAppVOByIdByAdminParams,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseAppVO>('/app/get', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  })
+}
+
+/** 此处后端没有提供注释 GET /app/get/my */
+export async function getMyApp(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getMyAppParams,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseAppVO>('/app/get/my', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  })
+}
+
+/** 此处后端没有提供注释 POST /app/list/good/page */
+export async function listGoodAppByPage(
+  body: API.AppQueryRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePageAppVO>('/app/list/good/page', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 此处后端没有提供注释 POST /app/list/my/page */
+export async function listMyAppVoByPage(
+  body: API.AppQueryRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePageAppVO>('/app/list/my/page', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 此处后端没有提供注释 POST /app/update/my */
+export async function updateMyApp(body: API.AppUpdateRequest, options?: { [key: string]: any }) {
+  return request<API.BaseResponseBoolean>('/app/update/my', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
